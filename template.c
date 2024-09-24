dacac3d0751a7468fa44f86e845482d7b5c62f79
Michael Hupfer <mhupfer@blackberry.com>
2024-09-24 09:45:03 +0200

    template.c

---
+++ template.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+// #include <sys/neutrino.h>
+// #include <pthread.h>
+// #include <sys/resource.h>
+// #include <sys/mman.h>
+// #include <time.h>
+// #include <assert.h>
+
+//build: 
+
+#define failed(f, e) fprintf(stderr, "%s:%d: %s() failed: %s\n", __func__, __LINE__, #f, strerror(e))
+
+
+
+/********************************/
+/* main							*/
+/********************************/
+int main(int argn, char* argv[]) {
+}
