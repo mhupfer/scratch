4a2c60526fb48eda9822067d1b0337213ab3c8ee
Michael Hupfer <mhupfer@blackberry.com>
2024-12-12 09:28:22 +0100

    test_dumper_event template.c

--- template.c
+++ template.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <stdbool.h>
 // #include <sys/neutrino.h>
 // #include <pthread.h>
 // #include <sys/resource.h>
@@ -16,11 +17,22 @@
 
 #define failed(f, e) fprintf(stderr, "%s:%d: %s() failed: %s\n", __func__, __LINE__, #f, strerror(e))
 
+#define usleep_ms(t) usleep((t*1000UL))
+
+// #define PASSED  "\033[92m"
+// #define FAILED "\033[91m"
+// #define ENDC  "\033[0m"
+
+// #define test_failed {\
+//     printf(FAILED"Test FAILED"ENDC" at %s, %d\n", __func__, __LINE__);\
+//     exit(1);\
+// }
 
 
 /********************************/
 /* main							*/
 /********************************/
 int main(int argn, char* argv[]) {
+    // printf(PASSED"Test PASS"ENDC"\n");
     return EXIT_SUCCESS;
 }
--- test_dumper_event.c
+++ test_dumper_event.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <stdbool.h>
 // #include <sys/neutrino.h>
 // #include <pthread.h>
 // #include <sys/resource.h>
@@ -16,16 +17,25 @@
 
 #define failed(f, e) fprintf(stderr, "%s:%d: %s() failed: %s\n", __func__, __LINE__, #f, strerror(e))
 
+#define PASSED  "\033[92m"
+#define FAILED "\033[91m"
+#define ENDC  "\033[0m"
+
+#define test_failed {\
+    printf(FAILED"Test FAILED"ENDC" at %s, %d\n", __func__, __LINE__);\
+    exit(1);\
+}
+
+#define usleep_ms(t) usleep((t*1000UL))
+
 #define DUMP_PULSE_CODE _PULSE_CODE_MINAVAIL
 
 int register_dumper_event(int coid);
+int cont_term(pid64_t pid, int status);
 int spawn_crash();
 int recv_pulse_to(int chid, struct _pulse *pulse, unsigned to_ms);
-
-#define test_failed {\
-    printf("Test FAILED at %s, %d\n", __func__, __LINE__);\
-    exit(1);\
-}
+int get_proc_info(pid64_t pid, debug_process_t *info) ;
+bool check_proc_exist(pid64_t pid);
 
 
 /********************************/
@@ -52,7 +62,7 @@ int main(int argn, char* argv[]) {
 
     if (res == -1) {
         system("slay dumper");
-        usleep(10000);
+        usleep_ms(10);
 
         res = register_dumper_event(my_coid);
 
@@ -62,7 +72,7 @@ int main(int argn, char* argv[]) {
         }
     }
 
-    /* check registering dunper twice */
+    /* check registering dumper twice */
     res = register_dumper_event(my_coid);
 
     if (res != -1 || errno != EBUSY) {
@@ -70,13 +80,13 @@ int main(int argn, char* argv[]) {
     }
 
     /* check if the correct pid is received*/
-    int child_pid = spawn_crash();
+    int crash_pid = spawn_crash();
     struct _pulse pulse;
 
     res = recv_pulse_to(my_chid, &pulse, 1000);
 
     if (res == EOK) {
-        if ((uintptr_t)pulse.value.sival_ptr != (uintptr_t)child_pid) {
+        if ((uintptr_t)pulse.value.sival_ptr != (uintptr_t)crash_pid) {
             test_failed;
         }
     } else {
@@ -84,7 +94,89 @@ int main(int argn, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    printf("Test PASS\n");
+    /* the process should still exist */
+    if (!check_proc_exist(crash_pid)) {
+        test_failed;
+    }
+
+    if (cont_term(crash_pid, 1) != EOK) {
+        failed(cont_term, errno);
+        return EXIT_FAILURE;
+    }
+
+    usleep_ms(10);
+    /* the process should have gone */
+    if (check_proc_exist(crash_pid)) {
+        test_failed;
+    }
+
+    /**
+     * create 2 crashees, don't terminate them
+     * exit this process, the 2 processes should  
+     * be cleaned up and the dumper event beeing
+     * unregistered
+     */
+    crash_pid = spawn_crash();
+    int crash_pid2 = spawn_crash();
+
+    res = recv_pulse_to(my_chid, &pulse, 1000);
+
+    if (res != EOK) {
+        failed(recv_pulse_to, errno);
+        return EXIT_FAILURE;
+    }
+
+    if ((uintptr_t)pulse.value.sival_ptr != (uintptr_t)crash_pid) {
+        test_failed;
+    }
+
+    res = recv_pulse_to(my_chid, &pulse, 1000);
+
+    if (res != EOK) {
+        failed(recv_pulse_to, errno);
+        return EXIT_FAILURE;
+    }
+
+    if ((uintptr_t)pulse.value.sival_ptr != (uintptr_t)crash_pid2) {
+        test_failed;
+    }
+
+    int child_pid = fork();
+
+    switch (child_pid)
+    {
+    case -1:
+        failed(fork, errno);
+        return EXIT_FAILURE;
+        break;
+    case 0:
+        break;
+    default:
+        /* parent just exits */
+        return EXIT_SUCCESS;
+        break;
+    }
+
+    usleep_ms(20);
+
+    if (check_proc_exist(getppid())) {
+        test_failed;
+    }
+    if (check_proc_exist(crash_pid)) {
+        test_failed;
+    }
+
+    if (check_proc_exist(crash_pid2)) {
+        test_failed;
+    }
+
+    res = register_dumper_event(my_coid);
+
+    if (res == -1) {
+        test_failed;
+    }
+
+    printf(PASSED"Test PASS"ENDC"\n");
     return EXIT_SUCCESS;
 }
 
@@ -104,6 +196,20 @@ int register_dumper_event(int coid) {
         exit(EXIT_FAILURE);
     }
 
+    return MsgSend(PROCMGR_COID, &msg, sizeof(msg), NULL, 0);
+}
+
+
+/********************************/
+/* cont_term                    */
+/********************************/
+int cont_term(pid64_t pid, int status) {
+    proc_coredump_t	msg;
+    msg.i.type = _PROC_COREDUMP;
+    msg.i.subtype = _PROC_COREDUMP_CONT_TERM;
+    msg.i.pid = pid;
+    msg.i.flags = status == EOK ? FLAG_PROC_CORDEDUMP_SUCCESS : 0;
+    
     return MsgSend(PROCMGR_COID, &msg, sizeof(msg), NULL, 0);
 }
 
@@ -139,4 +245,29 @@ int recv_pulse_to(int chid, struct _pulse *pulse, unsigned to_ms) {
     }
 
     return MsgReceivePulse(chid, pulse, sizeof(*pulse), NULL);
+}
+
+
+/********************************/
+/* get_proc_info                */
+/********************************/
+int
+get_proc_info(pid64_t pid, debug_process_t *info) {
+    struct _proc_debug i;
+    i.type = _PROC_DEBUG;
+    i.subtype = _PROC_DEBUG_PROC_INFO;
+    i.pid = pid;
+
+    int res = (int)-MsgSend_r(PROCMGR_COID, &i, sizeof(i), info, sizeof(*info));
+
+    return res;
+}
+
+/********************************/
+/* check_proc_exist             */
+/********************************/
+bool check_proc_exist(pid64_t pid) {
+    debug_process_t dinfo;
+
+    return get_proc_info(pid, &dinfo) == EOK;
 }
